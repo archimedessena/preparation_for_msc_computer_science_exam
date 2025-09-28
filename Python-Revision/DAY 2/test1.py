3dc699cb557b9b315e0a6a6314f680dd7393bf7a
Archimedes Senadju <archimedessena@gmail.com>
2025-09-28 19:32:46 +0000

    remove_added

--- Python-Revision/DAY 2/test1.py
+++ Python-Revision/DAY 2/test1.py
@@ -15,17 +15,28 @@ def view(task):
         print("Your tasks are: ", i)
    # print(task)
   
-def remove_task(task):
-    for i in task:
-        task[i].pop()
-    return task
-        
-
-
-
+             
+def remove_task(tasks):
+    while True:
+        ask = input("Do you want to remove any task? (yes/no): ").lower()
+        if ask == 'no':
+            break
+        elif ask == 'yes':
+            task_name = input("Enter the task you want to remove: ")
+            if task_name in tasks:
+                tasks.remove(task_name)
+                print(f"Task '{task_name}' removed successfully!")
+            else:
+                print("Task not found")
+        else:
+            print("Please enter 'yes' or 'no'")
+    
+    print("Updated task list:", tasks)           
+       
 def mark_as_done():
     pass
     
     
 add_task()
 view(task)
+remove_task(task)
